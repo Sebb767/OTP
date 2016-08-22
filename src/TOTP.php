4ee659cde6fcbc1b31b2c23200a2f0dd6630c0b7
Sebb767 <sebb@sebb767.de>
2016-08-22 23:30:32 +0200

    Added secret bytes variable and static function to create a new instance with a random secret

--- src/TOTP.php
+++ src/TOTP.php
@@ -7,25 +7,50 @@
  */
 
 namespace OTPLock;
+use Base32\Base32;
 
 
 class TOTP
 {
+    /**
+     * @var int Length of the secret (bytes).
+     */
+    public static $secretLength = 16;
+
     /*
-     * @var string The TOTP secret.
+     * @var string The TOTP secret (base32-encoded).
      */
     protected $secret;
 
     /**
+     * @var string The TOTP secret (in bytes).
+     */
+    protected $secretBytes;
+
+    /**
      * TOTP constructor.
-     * @param string $secret create a new class instance.
+     * @param string $secret The base32-encoded secret
      */
     public function __construct($secret)
     {
         $this->secret = $secret;
+        $this->secretBytes = Base32::decode($secret);
     }
 
-    
+    /**
+     * Creates a new class with a random secret
+     * @return TOTP
+     */
+    public static function createWithRandomSecret()
+    {
+        $secret = Base32::encode(random_bytes(self::$secretLength));
+
+        return new self($secret);
+    }
+
+    //
+    // --- getters + setters --
+    //
 
     /**
      * @return string
@@ -41,5 +66,23 @@ class TOTP
     public function setSecret($secret)
     {
         $this->secret = $secret;
+        $this->secretBytes = Base32::decode($secret);
+    }
+
+    /**
+     * @return string
+     */
+    public function getSecretBytes()
+    {
+        return $this->secretBytes;
+    }
+
+    /**
+     * @param string $secretBytes
+     */
+    public function setSecretBytes($secretBytes)
+    {
+        $this->secretBytes = $secretBytes;
+        $this->secret = Base32::encode($secretBytes);
     }
 }
